2ef13c22ac05c2e07ac8b121aa553b971ce606a9
Aidan Drekic <raptorsbasketball31@outlook.com>
2025-04-12 15:55:46 -0400

    added husky pre-commit.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import js from "@eslint/js"
-import globals from "globals"
+import reactPlugin from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
-import reactPlugin from "eslint-plugin-react"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
 import tseslint from "typescript-eslint"
 
 export default tseslint.config(
