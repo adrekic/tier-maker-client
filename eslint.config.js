a3fd2fcffb52d7c9efe46c5d48414d10856c5043
Aidan Drekic <raptorsbasketball31@outlook.com>
2025-04-12 15:49:56 -0400

    npx vite create, eslint set up, tailwind set up.

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import reactPlugin from "eslint-plugin-react"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+	{ ignores: ["dist"] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ["**/*.{ts,tsx,js}"],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			react: reactPlugin,
+			"react-hooks": reactHooks,
+			"react-refresh": reactRefresh,
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react/react-in-jsx-scope": "off",
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "error",
+			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					varsIgnorePattern: "^_",
+					argsIgnorePattern: "^_",
+					caughtErrorsIgnorePattern: "^_",
+				},
+			],
+		},
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+	},
+)
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <div className="flex w-full">App</div>
+}
+
+export default App
